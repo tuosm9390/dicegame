c8b2afe2520cc348bcbb735a2bbdec56bd497ff1
tuosm9390 <tuosm123@naver.com>
2022-02-22 15:49:07 +0900

    마무리

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Board from './Board';
+import Button from './button';
 import './App.css';
+import AppLogo from './assets/logo.png'
+
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 function App() {
+  const [myHistory, setMyHistory] = useState([]);
+  const [otherHistory, setOtherMyHistory] = useState([]);
+
+  const handleRollclick = () => {
+    const nextMyNum = random(6);
+    const nextOtherNum = random(6);
+    setMyHistory([...myHistory, nextMyNum]);  // ... -> 이전 배열을 불러옴
+    setOtherMyHistory([...otherHistory, nextOtherNum]);  // ... -> 이전 배열을 불러옴
+  };
+
+  const handleclearClick = () => {
+    setMyHistory([]);
+    setOtherMyHistory([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <img class="App-logo" src={AppLogo} alt="주사위게임 로고" />
+        <h1 class="App-title">주사위게임</h1>
+        <div>
+          <Button className='App-button' onClick={handleRollclick} color="blue">던지기</Button>
+          <Button className='App-button' onClick={handleclearClick} color="red">처음부터</Button>
+        </div>
+      </div>
+      <div>
+        <Board name="나" color="blue" gameHistory={myHistory}/>
+        <Board name="상대" color="red" gameHistory={otherHistory}/>
+      </div>
     </div>
   );
 }
